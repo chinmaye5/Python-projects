c6b224a027b66533a28ae8538fb80d61d709a53d
Pravesh-Sudha <programmerpravesh@gmail.com>
2025-02-19 13:34:14 +0530

    (Code): Minor tweaks

--- flask-todo-app/config.py
+++ flask-todo-app/config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'bhaiyokabhaipraveshbhai')
     AWS_REGION = 'us-east-1'
     DYNAMODB_TABLE = 'todo_tasks'
-    SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:624448302051:task-notificatoin'
+    SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:624448302051:task-notification'
     EMAIL = "programmerpravesh@gmail.com"
--- flask-todo-app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from tasks import tasks  # Import the tasks Blueprint
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'your-unique-secret-key'
-# Register the tasks Blueprint
-app.register_blueprint(tasks, url_prefix='/')
