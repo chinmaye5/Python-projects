58c354ffa4cfb9d31997159c3bcee048d4da1c70
Pravesh-Sudha <programmerpravesh@gmail.com>
2025-02-19 15:00:17 +0530

    minor tweaks

--- flask-todo-app/config.py
+++ flask-todo-app/config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'bhaiyokabhaipraveshbhai')
     AWS_REGION = 'us-east-1'
     DYNAMODB_TABLE = 'todo_tasks'
-    SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:624448302051:task-notification'
+    SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
     EMAIL = "programmerpravesh@gmail.com"
