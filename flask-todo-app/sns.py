a7b9f1271a41f196561aa0b3c4ed60af58dfa4ab
Pravesh-Sudha <programmerpravesh@gmail.com>
2025-02-19 17:10:15 +0530

    minor tweaks

--- flask-todo-app/sns.py
+++ flask-todo-app/sns.py
@@ -6,7 +6,7 @@ sns = boto3.client('sns', region_name=Config.AWS_REGION)
 def send_task_completion_email(email, task_description):
     message = f'Task completed: {task_description}'
     sns.publish(
-        TopicArn=f'{Config.SNS_TOPIC_ARN}',  # Replace with direct email ARN
+        TopicArn=Config.SNS_TOPIC_ARN,  # Replace with direct email ARN
         Message=message,
         Subject= "Task Completion Notification"
     )
