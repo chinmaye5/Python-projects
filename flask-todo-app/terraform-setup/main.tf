e98c341c07590ab48e8e9d6eaeab799e14317a3a
Pravesh-Sudha <88569706+Pravesh-Sudha@users.noreply.github.com>
2025-02-19 20:24:55 +0530

    Update main.tf

--- flask-todo-app/terraform-setup/main.tf
+++ flask-todo-app/terraform-setup/main.tf
@@ -25,7 +25,7 @@ resource "aws_sns_topic" "todo_notifications" {
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.todo_notifications.arn
   protocol  = "email"
-  endpoint  = "programmerpravesh@gmail.com"
+  endpoint  = "programmerpravesh@gmail.com"   # Change this your email
 }
 
 output "sns_topic_arn" {
