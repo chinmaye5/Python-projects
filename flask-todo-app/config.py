f68797174da616b12df6074b1df37482432b6b65
Manikanta chidurala <118712862+Manikantachidurala@users.noreply.github.com>
2025-07-25 18:46:57 +0530

    Update config.py

--- flask-todo-app/config.py
+++ flask-todo-app/config.py
@@ -5,4 +5,4 @@ class Config:
     AWS_REGION = 'us-east-1'
     DYNAMODB_TABLE = 'todo_tasks'
     SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
-    EMAIL = "programmerpravesh@gmail.com"
+    EMAIL = "chiduralamanikanta34@gmail.com"
